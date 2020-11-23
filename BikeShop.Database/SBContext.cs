7424e51f08da3a6d3e62706d70883b98e39d69b7
Georgi Yanakiev <g.yanakiev@yahoo.com>
2020-11-23 01:12:22 +0000

    initial setup added entities and database installed entity framework

---
+++ BikeShop.Database/SBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeShop.Database
+{
+    public class SBContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ BikeShop.Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeShop.Entities
+{
+    public class Product : BaseEntity
+    {
+        public Category Category { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
